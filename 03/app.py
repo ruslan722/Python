bab4373c6142ccb51cde16cf0188de9ccaf1e8fb
name <raleksandrovic619@gmail.com>
2025-09-19 21:12:00 +0300

    .....

--- 03/app.py
+++ 03/app.py
@@ -10,11 +10,11 @@ root.configure(bg='#FFFFFF')
 
 # === Шапка приложения ===
 header = Label(root, text="Симулятор Светофора", font=("Helvetica", 14), bg="#0991D5", fg="white")
-header.place(x=0, y=0, width=800, height=50)  
+header.place(x=0, y=0, width=800, height=50)
 
 # === Логотип ===
 logo = PhotoImage(file="logo.png")
-logo = logo.subsample(30)        
+logo = logo.subsample(30)     
 logo_label = Label(root, image=logo, bg="#0991D5")
 logo_label.image = logo            
 logo_label.place(x=0, y=2)
