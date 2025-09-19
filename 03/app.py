d89bcd427cfc7c04e68f05158eec4ec641e4f8d1
name <raleksandrovic619@gmail.com>
2025-09-19 21:12:57 +0300

    ......

--- 03/app.py
+++ 03/app.py
@@ -14,9 +14,9 @@ header.place(x=0, y=0, width=800, height=50)
 
 # === Логотип ===
 logo = PhotoImage(file="logo.png")
-logo = logo.subsample(30)     
+logo = logo.subsample(30) 
 logo_label = Label(root, image=logo, bg="#0991D5")
-logo_label.image = logo            
+logo_label.image = logo        
 logo_label.place(x=0, y=2)
 
 # === Запуск приложения ===
